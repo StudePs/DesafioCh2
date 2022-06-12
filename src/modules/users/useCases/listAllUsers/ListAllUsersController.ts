40d0c05f3e54800a0a2a134671dcb223f1559978
Renato Carvalho <renato.carvalho@direct.one>
2022-06-12 16:32:59 -0300

    feat: showProfile

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -8,12 +8,14 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
     try {
-      const users = this.listAllUsersUseCase.execute({ user_id });
-      return response.json({ status_code: 200, body: { users } });
+      const users = this.listAllUsersUseCase.execute({
+        user_id: user_id.toString(),
+      });
+      return response.json(users);
     } catch (error) {
       return response
-        .status(401)
-        .json({ status_code: 401, error: { message: error.message } });
+        .status(400)
+        .json({ status_code: 400, error: { message: error.message } });
     }
   }
 }
